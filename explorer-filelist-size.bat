4b0d0b15d00b55363e55b269914955cc446b6fd0
aldi <haroro107@gmail.com>
2025-06-10 21:45:19 +0700

    Update script to generate a comprehensive file and folder list with sizes

--- explorer-filelist-size.bat
+++ explorer-filelist-size.bat
@@ -1,6 +1,6 @@
 @echo off
-echo Generating file list (files only with sizes)...
+echo Generating file and folder list (with sizes)...
 
-powershell -Command "Get-ChildItem -Path . -Recurse -File | %% { $size = $_.Length; $formattedSize = if ($size -ge 1GB) { [math]::Round($size/1GB, 2).ToString() + 'GB' } elseif ($size -ge 1MB) { [math]::Round($size/1MB, 2).ToString() + 'MB' } elseif ($size -ge 1KB) { [math]::Round($size/1KB, 2).ToString() + 'KB' } else { $size.ToString() + 'B' }; \"$($_.FullName) [$formattedSize]\" }" > filelist.txt
+powershell -Command "Get-ChildItem -Path . -Recurse | %% { if ($_.PSIsContainer) { \"$($_.FullName) [DIR]\" } else { $size = $_.Length; $formattedSize = if ($size -ge 1GB) { [math]::Round($size/1GB, 2).ToString() + 'GB' } elseif ($size -ge 1MB) { [math]::Round($size/1MB, 2).ToString() + 'MB' } elseif ($size -ge 1KB) { [math]::Round($size/1KB, 2).ToString() + 'KB' } else { $size.ToString() + 'B' }; \"$($_.FullName) [$formattedSize]\" } }" > filelist.txt
 
-echo File list saved to filelist.txt
+echo File and folder list saved to filelist.txt
